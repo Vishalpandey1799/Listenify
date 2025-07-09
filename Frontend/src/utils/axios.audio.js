62d5c424121ac5fcad589f8a018598f90f4cddcb
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-07-09 08:03:25 +0530

    deeployeeeeeeeeeeeeeeeee

--- Frontend/src/utils/axios.audio.js
+++ Frontend/src/utils/axios.audio.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const audioInstance = axios.create({
-    baseURL : import.meta.env.VITE_SERVER_URL + "/api/listenify/create",
+    baseURL : import.meta.env.VITE_SERVER_URL + "api/listenify/create",
        headers :{
          "Content-Type" : "application/json"
     },
