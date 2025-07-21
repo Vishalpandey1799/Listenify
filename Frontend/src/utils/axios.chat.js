9add0e21f7d1d83abf59ceba754e59af7471496f
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-07-21 08:29:18 +0530

    url changes

--- Frontend/src/utils/axios.chat.js
+++ Frontend/src/utils/axios.chat.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const chatInstance = axios.create({
-    baseURL : import.meta.env.VITE_SERVER_URL + "user",
+    baseURL : import.meta.env.VITE_SERVER_URL + "api/listenify/user",
     withCredentials : true,
 })
--- Frontend/src/utils/axios.connection.js
+++ Frontend/src/utils/axios.connection.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-let baseurl = import.meta.env.VITE_SERVER_URL + "connection";
+let baseurl = import.meta.env.VITE_SERVER_URL + "api/listenify/connection";
 
 export const connectionAxios = axios.create({
     baseURL : baseurl,
